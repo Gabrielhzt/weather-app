27fb6e241f4f547918fceb3feb4dafa689b36d1c
Gabriel Hazout <gabrielhzt@Gabriels-MacBook-Pro.local>
2024-05-26 12:26:21 +0200

    Added the two boxes where the informations will be render

--- app/index.tsx
+++ app/index.tsx
@@ -21,7 +21,63 @@ export default function TabOneScreen() {
             <Text style={styles.text}>Precipitation</Text>
             <Text style={styles.text}>Max: 31° Min: 25°</Text>
           </View>
-          
+          <View style={styles.allbox}>
+            <View style={styles.box}>
+              <View style={styles.info}>
+                <Entypo name="water" size={18} color="#fff" />
+                <Text style={styles.textinfo}>6%</Text>
+              </View>
+              <View style={styles.info}>
+                <FontAwesome6 name="temperature-three-quarters" size={18} color="#fff" />
+                <Text style={styles.textinfo}>90%</Text>
+              </View>
+              <View style={styles.info}>
+                <Feather name="wind" size={24} color="#fff" />
+                <Text style={styles.textinfo}>19 km/h</Text>
+              </View>
+            </View>
+            <View style={styles.box2}>
+              <Text style={styles.description}>7-day Forecast</Text>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ flex: 1 }}>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 40, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 60, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 60, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 60, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 60, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 60, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={styles.day}>
+                    <Text style={styles.text}>28°C</Text>
+                    <Image source={require('@/assets/images/113.png')} style={{width: 60, height: 60}} />
+                    <Text style={styles.text}>15:00</Text>
+                  </View>
+                  <View style={{width: 25}}></View>
+              </ScrollView>
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </View>
@@ -46,5 +102,45 @@ const styles = StyleSheet.create({
   text: {
     color: "#fff",
   },
-  
+  allbox: {
+    flex: 1,
+    paddingHorizontal: 20,
+    gap: 20,
+    justifyContent: "flex-end"
+  },
+  box: {
+    backgroundColor: "rgba(0, 16, 38, 0.5)",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row"
+  },
+  box2: {
+    backgroundColor: "rgba(0, 16, 38, 0.5)",
+    paddingVertical: 20,
+    borderRadius: 20,
+    height: 200,
+    gap: 20
+  },
+  info: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center"
+  },
+  textinfo: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  description:{
+    color: "#fff",
+    paddingHorizontal: 20,
+  },
+  day: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginLeft: 25
+  }
 });
